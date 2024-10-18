5e28fa6a3fff2b577f09e41bcca5c4ee4c1b55b6
rosa <natsukiarr@gmail.com>
2024-10-17 17:47:13 -0700

    ADD endpoint paciente

--- api/api/src/main/java/org/vista/api/ControladorPaciente.java
+++ api/api/src/main/java/org/vista/api/ControladorPaciente.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import interfaces.INegocioPaciente;
+import org.rosa.negocioclinica.NegocioPaciente;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  *
@@ -26,6 +28,14 @@ public class ControladorPaciente {
         return response;
         
         
+    }
+    
+    @GetMapping("/")
+    public String obtenerPacientePsicologo(@RequestParam("id") Long id) {
+        
+        INegocioPaciente negocio = new NegocioPaciente();
+        
+        return negocio.getPacientesPsicologo(id);
     }
     
    
