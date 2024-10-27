e8f07c02706d628af3c621d1753c6e47ddddca8e
rosa <natsukiarr@gmail.com>
2024-10-26 20:23:49 -0700

    BASELINE 0.0.2

--- api/api/src/main/java/org/vista/api/ControladorExpediente.java
+++ api/api/src/main/java/org/vista/api/ControladorExpediente.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,6 +48,15 @@ public class ControladorExpediente {
     public String registrarExpediente(@RequestBody String json){
         INegocioExpediente negocioExpediente = NegocioFactory.createInstanceExpediente();
         String response = negocioExpediente.registrarExpediente(json);
+        
+        return response;
+    }
+    
+    @PutMapping("/actualizar")
+    public String actualizarExpediente(@RequestBody String json) {
+        INegocioExpediente negocioExpediente = NegocioFactory.createInstanceExpediente();
+        
+        String response = negocioExpediente.actualizarExpediente(json);
         
         return response;
     }
